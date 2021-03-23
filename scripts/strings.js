b029376e8f8ecbb490ed327a396c4c5b5dd6928b
MarianoBF <mariano.b.fernandez@gmail.com>
2021-03-23 20:15:12 -0300

    title renamings and styles

--- scripts/strings.js
+++ scripts/strings.js
@@ -1,27 +1,33 @@
 const ES_STRINGS = {
   main_title: "Algunos proyectos desarrollados:",
   lang_switch: "Click here to switch to English",
+  pomodoro_name: "Demo Pomodoro",
   pomodoro_desc:
     "Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
+  markup_name: "Demo Markup",
   markup_desc: "Markup, un visualizador de texto editado con markup.",
+  gifos_name: "Demo Gifos",
   gifos_desc:
     "Un buscador de gif con la posibilidad de crear y subir uno propio.",
-  drum_name: "Bateria",
+  drum_name: "Demo Bateria",
   drum_desc: "Una batería de sonidos con control vía mouse y teclado.",
-  calc_name: "Calculadora",
-  calculator_desc: "Una calculadora sencilla.",
+  calc_name: "Demo Calculadora",
+  calc_desc: "Una calculadora sencilla.",
 };
 
 const EN_STRINGS = {
   main_title: "A few sample projects:",
   lang_switch: "Haz clic aquí para cambiar a español",
+  pomodoro_name: "Pomodoro Timer Demo",
   pomodoro_desc: "A pomodoro timer with breaks and work sessions",
+  markup_name: "Markup Demo",
   markup_desc: "An editor for real-time viewing of markup editing.",
+  gifos_name: "Gifos Demo",
   gifos_desc: "Gif searcher creator with upload to Giphy",
-  drum_name: "Drums",
+  drum_name: "Drums Demo",
   drum_desc: "A drum machine with mouse and keyboard controls",
-  calc_name: "Calculator",
-  calculator_desc: "A simple calculator",
+  calc_name: "Calculator Demo",
+  calc_desc: "A simple calculator",
 };
 
 const ES_CLASS_STRINGS = {
