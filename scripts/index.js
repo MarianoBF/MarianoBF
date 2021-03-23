7cf67a230a33a83040216071acb10a23159d3724
MarianoBF <mariano.b.fernandez@gmail.com>
2021-03-23 19:44:56 -0300

    added links to code

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,46 @@
-document.getElementById("lang_switch").addEventListener("click", languageSwitcher)
+document
+  .getElementById("lang_switch")
+  .addEventListener("click", languageSwitcher);
 
 window.onload = setInitialLanguage;
 
 let current_lang;
 
 function setInitialLanguage() {
-    current_lang = "EN"
-    languageSwitcher()
+  current_lang = "EN";
+  languageSwitcher();
 }
 
 function languageSwitcher() {
-    if (current_lang === "ES") {
-        console.log("a")
-        language_strings = EN_STRINGS
-        current_lang = "EN" 
-    } else if (current_lang === "EN") {
-        console.log("b")
-        language_strings = ES_STRINGS
-        current_lang = "ES"}
-    description_locations = Object.keys(language_strings);
-    description_strings = Object.values(language_strings);
+  if (current_lang === "ES") {
+    language_strings = EN_STRINGS;
+    language_class_strings = EN_CLASS_STRINGS;
+    current_lang = "EN";
+  } else if (current_lang === "EN") {
+    language_strings = ES_STRINGS;
+    language_class_strings = ES_CLASS_STRINGS;
+    current_lang = "ES";
+  }
+  description_locations = Object.keys(language_strings);
+  description_strings = Object.values(language_strings);
 
-    for (let i = 0; i<description_locations.length; i++) {
-        document.getElementById(description_locations[i]).innerText = description_strings[i]
+  for (let i = 0; i < description_locations.length; i++) {
+    document.getElementById(description_locations[i]).innerText =
+      description_strings[i];
+  }
+
+  classesToChange = Object.keys(language_class_strings);
+  valuesToChange = Object.values(language_class_strings);
+  console.log(classesToChange, valuesToChange)
+
+
+  for (let cTC = 0; cTC < classesToChange.length; cTC++) {
+    let elementsToChange = document.getElementsByClassName(
+      classesToChange[cTC]);
+      console.log(classesToChange, cTC)
+    for (let eTC = 0; eTC < elementsToChange.length; eTC++) {
+      elementsToChange[eTC].innerText = valuesToChange[cTC];
+      console.log(eTC, cTC, elementsToChange, valuesToChange)
     }
+  }
 }
--- scripts/strings.js
+++ scripts/strings.js
@@ -1,23 +1,32 @@
 const ES_STRINGS = {
-    main_title: "Algunos proyectos desarrollados:",
-    lang_switch: "Click here to switch to English",
-    pomodoro_desc:"Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
-    markup_desc: "Markup, un visualizador de texto editado con markup.",
-    gifos_desc: "Un buscador de gif con la posibilidad de crear y subir uno propio.",
-    drum_name: "Bateria",
-    drum_desc: "Una batería de sonidos con control vía mouse y teclado.",
-    calc_name: "Calculadora",
-    calculator_desc: "Una calculadora sencilla."
-}
+  main_title: "Algunos proyectos desarrollados:",
+  lang_switch: "Click here to switch to English",
+  pomodoro_desc:
+    "Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
+  markup_desc: "Markup, un visualizador de texto editado con markup.",
+  gifos_desc:
+    "Un buscador de gif con la posibilidad de crear y subir uno propio.",
+  drum_name: "Bateria",
+  drum_desc: "Una batería de sonidos con control vía mouse y teclado.",
+  calc_name: "Calculadora",
+  calculator_desc: "Una calculadora sencilla.",
+};
 
 const EN_STRINGS = {
-    main_title: "A few sample projects:",
-    lang_switch: "Haz clic aquí para cambiar a español",
-    pomodoro_desc:"A pomodoro timer with breaks and work sessions",
-    markup_desc: "An editor for real-time viewing of markup editing.",
-    gifos_desc: "Gif searcher creator with upload to Giphy",
-    drum_name: "Drums",
-    drum_desc: "A drum machine with mouse and keyboard controls",
-    calc_name: "Calculator",
-    calculator_desc: "A simple calculator"
-}
+  main_title: "A few sample projects:",
+  lang_switch: "Haz clic aquí para cambiar a español",
+  pomodoro_desc: "A pomodoro timer with breaks and work sessions",
+  markup_desc: "An editor for real-time viewing of markup editing.",
+  gifos_desc: "Gif searcher creator with upload to Giphy",
+  drum_name: "Drums",
+  drum_desc: "A drum machine with mouse and keyboard controls",
+  calc_name: "Calculator",
+  calculator_desc: "A simple calculator",
+};
+
+const ES_CLASS_STRINGS = {
+  code: " (código)",
+};
+const EN_CLASS_STRINGS = {
+  code: " (code)",
+};
