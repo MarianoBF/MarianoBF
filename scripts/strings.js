96a0df2c8d5f82b5602b0f919d75e9171efb26b8
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-10 15:53:00 -0300

    added cartas / card game

--- scripts/strings.js
+++ scripts/strings.js
@@ -6,6 +6,8 @@ const ES_STRINGS = {
     "Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
   markup_name: "Demo Markup",
   markup_desc: "Markup, un visualizador de texto editado con markup.",
+  cartas_name: "Demo Cartas",
+  cartas_desc: "Un juego de cartas a 3 rondas",
   gifos_name: "Demo Gifos",
   gifos_desc:
     "Un buscador de gif con la posibilidad de crear y subir uno propio.",
@@ -31,6 +33,8 @@ const EN_STRINGS = {
   pomodoro_desc: "A pomodoro timer with breaks and work sessions",
   markup_name: "Markup Demo",
   markup_desc: "An editor for real-time viewing of markup editing.",
+  cartas_name: "Cartas Demo",
+  cartas_desc: "A 3-round card game",
   gifos_name: "Gifos Demo",
   gifos_desc: "Gif searcher with the ability to create and upload your own one to Giphy.",
   drum_name: "Drums Demo",
