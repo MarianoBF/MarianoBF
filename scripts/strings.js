bd589c1051c9355f4ed2841d0bff2420fa6f0a55
Mariano <mariano.b.fernandez@gmail.com>
2022-01-22 11:36:28 -0300

    include cartas project

--- scripts/strings.js
+++ scripts/strings.js
@@ -3,37 +3,41 @@ const ES_STRINGS = {
   full_stack_title: "Proyectos Fullstack",
   front_end_title: "Proyectos Frontend",
   lang_switch: "Click here to switch to English",
-  pomodoro_name: "Demo Pomodoro",
-  pomodoro_desc:
-    "Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
-  dospa_name: "Demo Dospa",
-  dospa_desc: "Un juego de cartas al estilo del Uno",
+  cartas_name: "Demo Cartas",
+  cartas_desc: "Un juego de cartas a 3 rondas, con tests en Cypress",
+  compras_name: "Demo Compras Z",
+  compras_desc:
+    "Un sitio de compras que persiste carrito, envía confirmación vía email, autentica vía Firebase y lo usa para persistir datos.",
   countries_name: "Demo Países",
   countries_desc:
     "Lista países desde una API y devuelve datos del clima desde una segunda API.",
+  dospa_name: "Demo Dospa",
+  dospa_desc: "Un juego de cartas al estilo del Uno",
+  drum_name: "Demo Bateria",
+  drum_desc: "Una batería con control vía mouse y teclado.",
   gifos_name: "Demo Gifos",
   gifos_desc:
     "Un buscador de gif con la posibilidad de crear y subir uno propio.",
-  drum_name: "Demo Bateria",
-  drum_desc: "Una batería con control vía mouse y teclado.",
-  presupFront_name: "Demo Presupuestaré Front",
-  presupBack_name: "Demo Presupuestaré Back",
-  presupBack_docs: "\n Docs de la API",
-  presup_desc:
-    "Una herramienta para registrar gastos y gestionarlos con categorías",
   pedidosFront_name: "Demo Pedidos Front",
   pedidosBack_name: "Demo Pedidos Back",
   pedidosBack_docs: "\n Docs de la API",
   pedidos_desc:
     "Una API para ABM de pedidos, productos y usuarios, con autenticación vía JWT",
+  pomodoro_name: "Demo Pomodoro",
+  pomodoro_desc:
+    "Pomodoro, una implementación de un timer de sesiones de trabajo con pausas.",
+  presupFront_name: "Demo Presupuestaré Front",
+  presupBack_name: "Demo Presupuestaré Back",
+  presupBack_docs: "\n Docs de la API",
+  presup_desc:
+    "Una herramienta para registrar gastos y gestionarlos con categorías",
   tarjeteroFront_name: "Demo Tarjetero Front",
   tarjeteroBack_name: "Demo Tarjetero Back",
   tarjeteroBack_docs: "\n Docs de la API",
   tarjetero_desc: "Tarjetero, un sistema para registrar contactos",
-  compras_name: "Demo Compras Z",
-  compras_desc: "Un sitio de compras que persiste carrito, envía confirmación vía email, autentica vía Firebase y lo usa para persistir datos.",
   users_name: "Demo API Reqres.in",
-  users_desc: "ABM de un listado de la API con AntDesign, persiste modificaciones en local storage",
+  users_desc:
+    "ABM de un listado de la API con AntDesign, persiste modificaciones en local storage",
 };
 
 const EN_STRINGS = {
@@ -41,34 +45,39 @@ const EN_STRINGS = {
   full_stack_title: "Fullstack Projects",
   front_end_title: "Frontend Projects",
   lang_switch: "Haz clic aquí para cambiar a español",
-  pomodoro_name: "Pomodoro Timer Demo",
-  pomodoro_desc: "A pomodoro timer with breaks and work sessions",
+  cartas_name: "Cartas Demo",
+  cartas_desc: "A 3-round card game with tests in Cypress",
+  compras_name: "Shopping Z Demo",
+  compras_desc:
+    "Shopping web with cart persistence, email confirmation and Firebase authentication & data storage",
+  countries_name: "Countries Demo",
+  countries_desc:
+    "Lists countries from API and shows weather data from a second API",
   dospa_name: "Dospa Demo",
   dospa_desc: "A Uno-like card game",
-  countries_name: "Countries Demo",
-  countries_desc: "Lists countries from API and shows weather data from a second API",
+  drum_name: "Drums Demo",
+  drum_desc: "A drum machine with mouse and keyboard controls",
   gifos_name: "Gifos Demo",
   gifos_desc:
     "Gif searcher with the ability to create and upload your own one to Giphy.",
-  drum_name: "Drums Demo",
-  drum_desc: "A drum machine with mouse and keyboard controls",
-  presupFront_name: "Presupuestaré Front Demo",
-  presupBack_name: "Presupuestaré Back Demo",
-  presupBack_docs: "\n API Docs",
-  presup_desc: "A tool for registering and managing expenses",
   pedidosFront_name: "Pedidos Front Demo",
   pedidosBack_name: "Pedidos Back Demo",
   pedidos_desc:
     "A CRUD API for orders, users and products, with JWT authentication",
   pedidosBack_docs: "\n API Docs",
+  pomodoro_name: "Pomodoro Timer Demo",
+  pomodoro_desc: "A pomodoro timer with breaks and work sessions",
+  presupFront_name: "Presupuestaré Front Demo",
+  presupBack_name: "Presupuestaré Back Demo",
+  presupBack_docs: "\n API Docs",
+  presup_desc: "A tool for registering and managing expenses",
   tarjeteroFront_name: "Tarjetero Front Demo",
   tarjeteroBack_name: "Tarjetero Back Demo",
   tarjeteroBack_docs: "\n API Docs",
   tarjetero_desc: "Tarjetero, a contact manager",
-  compras_name: "Shopping Z Demo",
-  compras_desc: "Shopping web with cart persistence, email confirmation and Firebase authentication & data storage",
   users_name: "Reqres.in API Demo",
-  users_desc: "CRUD for ReqRes API with AntDesign, persists changes in local storage",
+  users_desc:
+    "CRUD for ReqRes API with AntDesign, persists changes in local storage",
 };
 
 const ES_CLASS_STRINGS = {
